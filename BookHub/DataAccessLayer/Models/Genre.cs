2af82f064850d49668d341b46d978373c8c5c8cd
Martin Gargalovič <xgargal@fi.muni.cz>
2023-10-09 20:20:37 +0200

    Created Entity for Genre and Connection Entity For Genre and Book

---
+++ BookHub/DataAccessLayer/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Genre : BaseEntity
+    {
+        public string Name { get; set; }
+        public List<int> GenreBookId { get; set; } = new List<int>();
+    }
+}
---
+++ BookHub/DataAccessLayer/Models/GenreBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class GenreBook : BaseEntity
+    {
+        public int BookId { get; set; }
+        public int GenreId { get; set; }
+    }
+}
