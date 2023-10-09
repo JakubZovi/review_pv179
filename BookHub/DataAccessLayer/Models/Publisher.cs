b5fc537149023eda6172306d2d00ec1d7fb31941
Martin Gargalovič <xgargal@fi.muni.cz>
2023-10-09 20:21:01 +0200

    Created Entity for a Publisher

---
+++ BookHub/DataAccessLayer/Models/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Publisher : BaseEntity
+    {
+        public string Name { get; set; }
+        public string EMail { get; set; }
+        public string BookId { get; set; }
+    }
+}
