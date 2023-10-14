55be7e716e684ad3f88dc729d5841f7f1b77f970
Martin Gargalovič <xgargal@fi.muni.cz>
2023-10-14 15:30:50 +0200

    Changed to the virtual list of GenreBooks

--- BookHub/DataAccessLayer/Models/Genre.cs
+++ BookHub/DataAccessLayer/Models/Genre.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataAccessLayer.Models
+﻿namespace DataAccessLayer.Models
 {
     public class Genre : BaseEntity
     {
         public string Name { get; set; }
-        public List<int> GenreBookId { get; set; } = new List<int>();
+        public virtual IEnumerable<GenreBook> GenreBooks { get; set; }
     }
 }
