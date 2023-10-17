110a8553858af6e0d9f998c523f920c00a42a0c7
Juraj Fiala <jurf@riseup.net>
2023-10-17 19:49:00 +0200

    feat: Seed Genre, Publisher and a few Books

--- BookHub/DataAccessLayer/Data/BookHubDBContext.cs
+++ BookHub/DataAccessLayer/Data/BookHubDBContext.cs
@@ -28,76 +28,7 @@ public class BookHubDBContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // Book - Publisher 1:m
-        modelBuilder.Entity<Book>()
-            .HasOne(book => book.Publisher)
-            .WithMany(publisher => publisher.Books)
-            .HasForeignKey(book => book.PublisherId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-
-        // Book - Price 1:m
-        modelBuilder.Entity<Price>()
-            .HasOne(price => price.Book)
-            .WithMany(book => book.Prices)
-            .HasForeignKey(price => price.BookId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        
-        // Rating - Book 1:m
-        modelBuilder.Entity<Rating>()
-            .HasOne(rating => rating.Book)
-            .WithMany(book => book.Ratings)
-            .HasForeignKey(rating => rating.BookId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        // Rating - User 1:m
-        modelBuilder.Entity<Rating>()
-            .HasOne(rating => rating.User)
-            .WithMany(user => user.Ratings)
-            .HasForeignKey(rating => rating.UserId)
-            .OnDelete(DeleteBehavior.Restrict);
-        
-        // User : Purchase 1:m
-        modelBuilder.Entity<Purchase>()
-            .HasOne(purchase => purchase.User)
-            .WithMany(user => user.Purchases)
-            .HasForeignKey(purchase => purchase.UserId)
-            .OnDelete(DeleteBehavior.Restrict);
-        
-        // Purchase : PurchaseBooks 1:m
-        modelBuilder.Entity<Purchase>()
-            .HasMany(purchase => purchase.PurchaseBooks)
-            .WithOne(purchaseBook => purchaseBook.Purchase)
-            .HasForeignKey(purchaseBook => purchaseBook.PurchaseId)
-            .OnDelete(DeleteBehavior.Restrict);
-        
-        // Book - PurchaseBooks 1:m
-        modelBuilder.Entity<Book>()
-            .HasMany(book => book.PurchaseBooks)
-            .WithOne(purchaseBook => purchaseBook.Book)
-            .HasForeignKey(purchaseBook => purchaseBook.BookId)
-            .OnDelete(DeleteBehavior.Restrict);
-        
-        
-        // Book - Author m:n
-        modelBuilder.Entity<Book>()
-            .HasMany(book => book.Author)
-            .WithMany(author => author.Books)
-            .UsingEntity("BookToAuthorJoin");
-
-        // Book - Genre m:n
-        modelBuilder.Entity<Book>()
-            .HasMany(book => book.Genres)
-            .WithMany(genre => genre.Books)
-            .UsingEntity("BookToGenreJoin");
-
-        // Book - Wishlist m:n
-        modelBuilder.Entity<Book>()
-            .HasMany(book => book.Wishlist)
-            .WithMany(wishlist => wishlist.Books)
-            .UsingEntity("BookToWishlistJoin");
-        
+        modelBuilder.Seed();
 
         base.OnModelCreating(modelBuilder);
     }
