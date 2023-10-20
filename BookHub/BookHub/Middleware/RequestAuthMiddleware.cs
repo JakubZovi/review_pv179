421c98e6d99b638d94c0ba80fd7da966c51f2fda
Martin Gargalovič <xgargal@fi.muni.cz>
2023-10-20 19:30:00 +0200

    feat: added AuthMiddleware

---
+++ BookHub/BookHub/Middleware/RequestAuthMiddleware.cs
@@ -0,0 +1,33 @@
+﻿namespace BookHub.Middleware;
+
+public class RequestAuthMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RequestAuthMiddleware> _logger;
+
+    public RequestAuthMiddleware(RequestDelegate next, ILogger<RequestAuthMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+    
+    public async Task Invoke(HttpContext context)
+    {
+
+        var InitialPass = "IBetYouCanNotGuessThisOne";
+        
+        var token = context.Request.Headers["Authorization"];
+
+        if (!token.Equals(InitialPass))
+        {
+            await context.Response.WriteAsync("Unauthorized");
+        }
+
+        else
+        {
+            await _next(context);
+        }
+
+        _logger.LogInformation($"Request token is {token}");
+    }
+}
